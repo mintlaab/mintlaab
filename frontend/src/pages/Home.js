3aca0971015e25c5f825be906dac092dc0a20eaa
Danny Chung <41158853+hachui@users.noreply.github.com>
2021-05-30 16:15:23 -0400

    ens onChange bug fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,13 +7,14 @@ import GET_ADDRESS from "../graphql/subgraph";
 
 function Query(props){
   const name = props.ens;
+  console.log(name)
   const { loading, error, data } = useQuery(GET_ADDRESS, {
     variables: {name}
   });
   let definedData = 'Not Found'
   if(!loading && !error){
-  definedData = data.domains[0].resolvedAddress.id
-  console.log(definedData)
+    definedData = data.domains[0].resolvedAddress.id
+    console.log(definedData)
   }
   return <p>ENS Saved For Address: {definedData}</p>
 }
@@ -34,13 +35,18 @@ const Home = () => {
   return (
     <div>
       <button className="btn btn-success" onClick={goToMint}>Create</button>
+      
       <label>Enter ENS name:</label>
       <input 
       type='text' 
       placeholder='ENS name' 
-      onChange={e => setEns(e.target.value)}>
+      onChange={e => {
+        if(!submitted){
+        setEns(e.target.value)
+        }
+      }}>
       </input>
-      <button onClick={() =>{
+      <button type='submit' onClick={() =>{
         setSubmitted(true)
       }}>Submit</button>
       {submitted ? <Query ens={ens}/> : <div></div>}
